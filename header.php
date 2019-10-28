829ad7734f54fba87831ecceb276437b8039fca7
Belsea <44635556+Belsea@users.noreply.github.com>
2019-10-28 10:21:56 +0100

    Adding partials - responsive background

--- header.php
+++ header.php
@@ -11,13 +11,7 @@
 
 <body <?php body_class(); ?>>
 
-    <img class="bg-image lazyload" src="<?php echo esc_url(home_url('/wp-content/themes/alisart-theme/images/bg-1440x810.jpg')); ?>" srcset="<?php echo esc_url(home_url('/wp-content/themes/alisart-theme/images/bg-1440x810.jpg')); ?> 1440w,
-                <?php echo esc_url(home_url('/wp-content/themes/alisart-theme/images/bg-1920x1080.jpg')); ?> 1920w,
-                <?php echo esc_url(home_url('/wp-content/themes/alisart-theme/images/bg-2880x1620.jpg')); ?> 2880w,
-                <?php echo esc_url(home_url('/wp-content/themes/alisart-theme/images/bg-3840x2160.jpg')); ?> 3840w,
-                <?php echo esc_url(home_url('/wp-content/themes/alisart-theme/images/bg-4800x2700.jpg')); ?> 4800w,
-                <?php echo esc_url(home_url('/wp-content/themes/alisart-theme/images/bg-5760x3240.jpg')); ?> 5760w" data-sizes="auto" ;>
-
+    <?php get_template_part('partials/responsive-bg') ?>
 
     <nav id="popup-nav" class="popup-nav">
         <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
