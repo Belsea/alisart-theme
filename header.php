07cc8c216c693c8c2ee101c3305bfca2660e6457
Belsea <44635556+Belsea@users.noreply.github.com>
2019-10-29 11:28:45 +0100

    Compacted code of popup-nav

--- header.php
+++ header.php
@@ -13,32 +13,4 @@
 
     <?php get_template_part('partials/responsive-bg') ?>
 
-    <nav id="popup-nav" class="popup-nav">
-        <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-            <img class="popup-nav__logo" src="<?php echo esc_url(home_url('/wp-content/themes/alisart-theme/images/logo-white.svg')); ?>" alt="Alice Rossi - makeup artist &amp; hair stylist">
-        </a>
-
-        <button id="popup-nav__toggler" class="popup-nav__toggler">
-            <div id="hamburger" class="popup-nav__toggler__hamburger"><span></span><span></span><span></span><span></span></div>
-        </button>
-
-
-        <div class="popup-nav__menu">
-            <?php
-            wp_nav_menu(array(
-                'theme_location' => 'popup-nav_pages',
-                'menu_id'       => '',
-                'menu_class'    => 'pages-nav m-0 p-0',
-                'container'     => ''
-            ));
-
-            wp_nav_menu(array(
-                'theme_location' => 'popup-nav_socials',
-                'menu_id'       => '',
-                'menu_class'    => 'nav-social-links m-0 p-0',
-                'container'     => ''
-            ));
-            ?>
-        </div>
-
-    </nav>
+    <?php get_template_part('partials/popup-nav') ?>
---
+++ partials/popup-nav.php
@@ -0,0 +1,58 @@
+<?php
+if (is_home()) :
+   $portraitMenu = "popup-nav_pages-categories";
+else :
+   $portraitMenu = "popup-nav_pages";
+endif
+?>
+
+<nav id="popup-nav" class="popup-nav">
+
+   <div class="popup-nav__subcontainer">
+      <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+         <img class="popup-nav__logo" src="<?php echo esc_url(home_url('/wp-content/themes/alisart-theme/images/logo-white.svg')); ?>" alt="Alice Rossi - makeup artist &amp; hair stylist">
+      </a>
+
+      <button id="popup-nav__toggler" class="popup-nav__toggler">
+         <div id="hamburger" class="popup-nav__toggler__hamburger"><span></span><span></span><span></span><span></span></div>
+      </button>
+   </div>
+
+   <div class="popup-nav__menu popup-nav__menu-portrait">
+      <?php
+      // ul class=sub-menu inside the li of portfolio
+      wp_nav_menu(array(
+         'theme_location' => $portraitMenu,
+         'menu_id'       => '',
+         'menu_class'    => 'popup-nav__menu-portrait__pages m-0 p-0',
+         'container'     => ''
+      ));
+
+      wp_nav_menu(array(
+         'theme_location' => 'popup-nav_socials',
+         'menu_id'       => '',
+         'menu_class'    => 'popup-nav__menu-portrait__socials m-0 p-0',
+         'container'     => ''
+      ));
+      ?>
+   </div>
+
+   <div class="popup-nav__menu popup-nav__menu-landscape">
+      <?php
+      wp_nav_menu(array(
+         'theme_location' => 'popup-nav_pages',
+         'menu_id'       => '',
+         'menu_class'    => 'popup-nav__menu-landscape__pages m-0 p-0',
+         'container'     => ''
+      ));
+
+      wp_nav_menu(array(
+         'theme_location' => 'popup-nav_socials',
+         'menu_id'       => '',
+         'menu_class'    => 'popup-nav__menu-landscape__socials m-0 p-0',
+         'container'     => ''
+      ));
+      ?>
+   </div>
+
+</nav>
